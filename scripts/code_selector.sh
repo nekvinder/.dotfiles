697993f66b1374fe732e5a973518d1786f456edb
nekvinder <nekvinder@gmail.com>
2023-06-25 01:59:47 +0530

    allow multiple project roots in code selector

--- scripts/code_selector.sh
+++ scripts/code_selector.sh
@@ -1,12 +1,18 @@
 #!/usr/bin/bash
 
-MyWorkDir="/home/nekvinder/wk"
+MyWorkDir="/home/nekvinder"
+selected=$(ls -t -d $MyWorkDir/{personalProjects,wk}/*/ | tr '\n' '\0' | xargs -0 -n 1 basename | rofi -dmenu -p "Select a code dir" -config ~/.config/rofi/rofidmenu.rasi)
 
-selected=$(ls -t -d $MyWorkDir/*/ | tr '\n' '\0' | xargs -0 -n 1 basename | rofi -dmenu -p "Select a code dir" -config ~/.config/rofi/rofidmenu.rasi)
 if [[ -z $selected  ]]; then
     echo "empty" >> /dev/null
 else
     # runTmux has to be called with full path as it is not in PATH when called from i3
-    $HOME/.dotfiles/scripts/runTmux.sh $MyWorkDir/$selected "$selected" >> /dev/null
-    alacritty --working-directory="$MyWorkDir/$selected" --title="$selected : NVim" -e $SHELL -lc "tmux -u attach -t $selected" >> /dev/null
+    if [ -d "$MyWorkDir/personalProjects/$selected" ]; then
+        SelectedDirPath="$MyWorkDir/personalProjects/$selected"
+    else
+        SelectedDirPath="$MyWorkDir/wk/$selected"
+    fi
+
+    $HOME/.dotfiles/scripts/runTmux.sh $SelectedDirPath "$selected" >> /dev/null
+    alacritty --working-directory="$SelectedDirPath" --title="$selected : NVim" -e $SHELL -lc "tmux -u attach -t $selected" >> /dev/null
 fi
